9335fc1cc17b2f88873cf3121c93efcfb0b276de
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-09 09:38:09 +0530

    MyJJ9-9

---
+++ MyyMicroservices_ba6i_Discovery_Service/src/test/java/com/discovery_service/MySpringBootBa6iDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootBa6iDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
