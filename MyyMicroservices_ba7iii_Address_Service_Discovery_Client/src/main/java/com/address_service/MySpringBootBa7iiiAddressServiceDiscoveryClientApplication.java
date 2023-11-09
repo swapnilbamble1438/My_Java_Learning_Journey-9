66b5020c8e5af13df37a6e51acd419e034259ab9
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-09 09:46:30 +0530

    MyJJ9-15

---
+++ MyyMicroservices_ba7iii_Address_Service_Discovery_Client/src/main/java/com/address_service/MySpringBootBa7iiiAddressServiceDiscoveryClientApplication.java
@@ -0,0 +1,13 @@
+package com.address_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootBa7iiiAddressServiceDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootBa7iiiAddressServiceDiscoveryClientApplication.class, args);
+	}
+
+}
