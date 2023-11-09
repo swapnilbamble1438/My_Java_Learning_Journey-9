d48dfc7f21e93ce187365d129ff43941a757be41
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-09 09:52:24 +0530

    MyJJ9-20

---
+++ MyyMicroservices_bb3_A_Microservice_Eureka_Server/src/test/java/com/eserver/MySpringBootBa1iiMicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootBa1iiMicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
