6708c6ef37ed64ca64c66ccbef7250f2eabda7eb
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-09 10:00:21 +0530

    j

--- MyyMicroservices_bb6_B_Microservice_User/src/main/java/com/user/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.user.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Long>{
-
-}
